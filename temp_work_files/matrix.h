5ab81a45b23792a0a8410dc62b2911aa564db05f
ujvarid <ujvaridavid2004@gmail.com>
2024-01-31 11:12:09 +0100

    par dolog

---
+++ temp_work_files/matrix.h
@@ -0,0 +1,22 @@
+#ifndef MTRX
+
+#define MTRX
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void matrix_free(int ** matrix, short height, short width);
+void matrix_generating(int *** matrix, short height, short width);
+void matrix_print(int ** matrix, short height, short width);
+void matrix_init(int *** matrix, short height, short width);
+void matrix_transpose(int *** matrix, short *height, short *width);
+void matrix_multiplication_lambda(int *** matrix, short height, short width);
+void matrix_addition_constant(int *** matrix, short height, short width);
+void matrix_addition(int *** matrix, short height, short width);
+void matrix_input(int *** matrix, short * height, short * width);
+void matrix_welcome();
+void matrix_choice_input(short * choice);
+void matrix_directing();
+
+#endif
